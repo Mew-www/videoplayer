b970723be6bdefaa337c559fd3a853b108998222
jade <jasmiinv@metropolia.fi>
2017-09-15 13:32:12 +0300

    Testing videolist

--- sources/js/videolist/videolist.js
+++ sources/js/videolist/videolist.js
@@ -1,14 +1,22 @@
-
+let listing = [];
 // get auto-generated page
 $.get('http://62.78.180.5/streamprojekti/').then(function(html) {
     // create temporary DOM element
-   let document = $(html);
+    let document = $(html);
 
     // find all links ending with .pdf
-    document.find('a[href$=".wov"]').each(function() {
-        let pdfName = $(this).text();
-        let pdfUrl = $(this).attr('href');
+    document.find('a[href$=".mov"]').each(function() {
+       // console.log($(this).attr("href"));
+        //console.log($(this).parent().next().html());
+        //console.log($(this).parent().next().next().html());
+        let movie = {
+            name:$(this).attr("href"),
+            last_modified:$(this).parent().next().html(),
+            size:$(this).parent().next().next().html()
+        };
+        listing.push(movie);
+        console.log(movie);
 
-        // do what you want here
-    })
+    });
+    console.log(listing);
 });

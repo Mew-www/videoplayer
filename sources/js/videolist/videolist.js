00ff4c4fae64445249ad3f2593bc1ad0821d17c4
jade <jasmiinv@metropolia.fi>
2017-09-20 12:53:51 +0300

    testing

--- sources/js/videolist/videolist.js
+++ sources/js/videolist/videolist.js
@@ -1,21 +1,29 @@
-let listing = [];
+
+    let listing = [];
 // get auto-generated page
-$.get('http://62.78.180.5/streamprojekti/').then(function(html) {
-    // create temporary DOM element
-    let document = $(html);
+    $.get('http://62.78.180.5/streamprojekti/').then(function (html) {
+        // create tempovrary DOM element
+        let document = $(html);
 
-    document.find('a[href$=".mov"]').each(function() {
-       // console.log($(this).attr("href"));
-        //console.log($(this).parent().next().html());
-        //console.log($(this).parent().next().next().html());
-        let movie = {
-            name:$(this).attr("href"),
-            last_modified:$(this).parent().next().html(),
-            size:$(this).parent().next().next().html()
-        };
-        listing.push(movie);
-        console.log(movie);
+        document.find('a[href$=".mov"]').each(function () {
+            // console.log($(this).attr("href"));
+            //console.log($(this).parent().next().html());
+            //console.log($(this).parent().next().next().html());
+            let movie = {
+                name: $(this).attr("href"),
+                last_modified: $(this).parent().next().html(),
+                size: $(this).parent().next().next().html()
+            };
+            listing.push(movie);
 
+        });
+
+
+        for(let i = 0; i < listing.length; i++) {
+            console.log(listing[i].name);
+            console.log(listing[i]);
+       
+        }
+        console.log(listing[2]);
+        $("#videolist").html("<ol><li>jotain</li><li>jotain2</li></ol>");
     });
-    console.log(listing);
-});

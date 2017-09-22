1d63372e9134c63255b0e71e460f3caa16dc05be
Mew-www <mew.proxy@hotmail.com>
2017-09-22 15:15:06 +0300

    Finished initial videoplayer.js (classes Movie & Player), implemented movietheaterlike background fade.

--- sources/js/videoplayer/videoplayer.js
+++ sources/js/videoplayer/videoplayer.js
@@ -1,19 +1,49 @@
-jwplayer('videoplayer').setup({
-  file: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  image: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Big_buck_bunny_poster_big.jpg",
-  height: 360,
-  width: 640,
-});
+class Movie {
 
-let dummy_movie_object = {
-  title: "american-made-trailer-1_h1080p.mov",
-  description: "This has lots of kittens in it!",
-  sources: [
+  constructor(title, description, sources) {
+    this.title = title;
+    this.description = description;
+    this.sources = sources;
+  }
+
+}
+
+class Player {
+
+  constructor(jwplayer, list_of_movies) {
+    if (!list_of_movies.every(movie => movie instanceof Movie)) {
+      throw Error('Bad argument for Player constructor. Given argument was not instance of Movie -class.');
+    }
+    if (list_of_movies.length < 1) {
+      throw Error('Bad argument for Player constructor. List of movies had length less than 1.')
+    }
+    this.movies = list_of_movies;
+    this.jwplayer = jwplayer;
+    jwplayer.setup({
+      height: 360,
+      width: 640,
+      file: list_of_movies[0].sources.find(source=>source.label==="Full HD").file,
+      title: list_of_movies[0].title,
+      description: list_of_movies[0].description
+    });
+  }
+
+  play(movie) {
+    if (!(movie instanceof Movie)) {
+      throw Error('Bad argument for function changeMovie. Given argument was not instance of Movie -class.');
+    }
+    this.jwplayer.load([movie]);
+  }
+
+}
+
+let movielist = [1,2,3,4,5].map(num => new Movie(
+  "american-made-trailer-1_h1080p.mov",
+  "This has lots of kittens in it!", [
     {file: 'http://62.78.180.5/streamprojekti/american-made-trailer-1_h1080p.mov', label: 'Full HD'},
     {file: 'http://62.78.180.5/streamprojekti/american-made-trailer-1_h480p.mov', label: 'Low quality'}
-  ]
-};
-
-function changeTest() {
-  jwplayer('videoplayer').load([dummy_movie_object]);
-}
+  ])
+);
+let player = new Player(window.jwplayer('videoplayer'), movielist);
+player.jwplayer.on('play', ()=>{$("body").css('background', "black");});
+player.jwplayer.on('pause', ()=>{$("body").css('background', "white");});

f1b76667dcc80119f73262c68eab1adbcdb9f7b1
jade <jasmiinv@metropolia.fi>
2017-09-22 12:15:14 +0300

    videolist with divs

--- sources/js/videolist/videolist.js
+++ sources/js/videolist/videolist.js
@@ -18,15 +18,29 @@
 
         });
 
+        let div1 = $( "<div class='movie-wrapper'></div>" );
+        $("#videolist").append(div1);
+
 
         for(let i = 0; i < listing.length; i++) {
-            /*console.log(listing[i].name);
-            console.log(listing[i]);*/
+            console.log(listing[i].name);
+            console.log(listing[i]);
 
-            $("#videolist").append(listing[i].name, [2] ); //näkyy kaikki leffat
-               // "</li><li>jotain2</li></ol>");
+
+            div1.append("<div class='movie-element'>"+
+                    listing[i].name +
+                    "<div class='movie-info'>" + listing[i].last_modified + "</div>" +
+                "</div>");
+            //$("#videolist").append( listing[i].name );
+
 
         }
+
+
+        //näkyy kaikki leffat
+               // "</li><li>jotain2</li></ol>");
+
+
         //console.log(listing[2]);
        // $("#videolist").append("<ol><li>"[i]"</li><li>jotain2</li></ol>");
     });
